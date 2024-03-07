7078918c1b0edd7dd7e5d9980ab3f3488e7064f8
Kim YoonSu <121972417+yoonsu417@users.noreply.github.com>
2024-03-07 15:46:31 +0900

    테이블 생성, 조회

---
+++ SQL workbench/CREATE TABLE, SELECT.sql
@@ -0,0 +1,91 @@
+DROP DATABASE IF EXISTS market_db;
+
+CREATE DATABASE market_db;
+USE market_db;
+CREATE TABLE member
+(	mem_id CHAR(8) NOT NULL PRIMARY KEY,
+	mem_name VARCHAR(10) NOT NULL,
+    mem_number INT NOT NULL,
+    addr CHAR(2) NOT NULL,
+    phone1 CHAR(3),
+    phone2 CHAR(8),
+    height SMALLINT,
+    debut_date date
+);
+
+CREATE TABLE buy
+(	num INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	mem_id CHAR(8) NOT NULL,
+    prod_name CHAR(6) NOT NULL,
+    group_name CHAR(4),
+    price INT NOT NULL,
+    amount SMALLINT NOT NULL,
+    FOREIGN KEY(mem_id) REFERENCES member(mem_id)
+    );
+
+INSERT INTO member VALUES('TWC', '트와이스', 9, '서울', '02', '11111111', 167, '2015.10.19');
+INSERT INTO member VALUES('BLK', '블랙핑크', 4, '경남', '055', '22222222', 163, '2016.08.08');
+INSERT INTO member VALUES('WMN', '여자친구', 6, '경기', '031', '33333333', 166, '2015.01.15');
+INSERT INTO member VALUES('OMY', '오마이걸', 7, '서울', NULL, NULL, 160, '2015.04.21');
+INSERT INTO member VALUES('GRL', '소녀시대', 8, '서울', '02', '44444444', 168, '2007.08.02');
+INSERT INTO member VALUES('ITZ', '잇지', 5, '경남', NULL, NULL, 167, '2019.02.12');
+INSERT INTO member VALUES('RED', '레드벨벳', 4, '경북', '054', '55555555', 161, '2014.08.01');
+INSERT INTO member VALUES('APN', '에이핑크', 6, '경기', '031', '77777777', 164, '2011.02.10');
+INSERT INTO member VALUES('SPC', '우주소녀', 13, '서울', '02', '88888888', 162, '2016.02.25');
+INSERT INTO member VALUES('MMU', '마마무', 4, '전남', '061', '99999999', 165, '2014.06.19');
+
+INSERT INTO buy VALUES(NULL, 'BLK', '지갑', NULL, 30, 2);
+INSERT INTO buy VALUES(NULL, 'BLK', '맥북프로', '디지털', 1000, 1);
+INSERT INTO buy VALUES(NULL, 'APN', '아이폰', '디지털', 200, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '아이폰', '디지털', 200, 5);
+INSERT INTO buy VALUES(NULL, 'BLK', '청바지', '패션', 50, 3);
+INSERT INTO buy VALUES(NULL, 'MMU', '에어팟', '디지털', 80, 10);
+INSERT INTO buy VALUES(NULL, 'GRL', '혼공SQL', '서적', 15, 5);
+INSERT INTO buy VALUES(NULL, 'APN', '혼공SQL', '서적', 15, 2);
+INSERT INTO buy VALUES(NULL, 'APN', '청바지', '패션', 50, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '지갑', NULL, 30, 1);
+INSERT INTO buy VALUES(NULL, 'APN', '혼공SQL', '서적', 15, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '지갑', NULL, 30, 4);
+
+SELECT * FROM member;
+SELECT * FROM buy;
+
+SELECT * FROM MEMBER;
+SELECT mem_id, mem_name from MEMBER;
+
+SELECT mem_name, mem_id, phone1 from MEMBER;
+SELECT * FROM market_db.member;
+
+SELECT mem_id, addr, mem_name FROM member WHERE mem_name = '에이핑크';
+SELECT mem_id, addr, mem_name FROM member WHERE addr = '전남';
+
+SELECT * FROM member where height >= 165;
+
+SELECT * FROM member WHERE height >= 165 or mem_number >= 6;
+
+SELECT * FROM member WHERE addr in ('서울', '경기');
+
+SELECT * FROM member WHERE mem_name like '%무';
+
+SELECT * FROM member WHERE mem_name like '__무';
+
+SELECT * FROM member WHERE mem_name like '__핑크';
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
